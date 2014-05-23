e7b92b3540c2a31ca086c553f318dff9b05b200c
David Ding <chengjie.ding@gmail.com>
2014-05-24 07:43:05 +0800

    add JSON api

--- SpringMVCApp/src/main/java/com/springapp/mvc/UserController.java
+++ SpringMVCApp/src/main/java/com/springapp/mvc/UserController.java
@@ -1,16 +1,17 @@
 package com.springapp.mvc;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -35,5 +36,21 @@ public class UserController {
         userRepository.delete(userRepository.findOne(userId));
 
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String listUsersJson(ModelMap model) throws JSONException {
+        JSONArray userArray = new JSONArray();
+        for (User user : userRepository.findAll()) {
+            JSONObject userJSON = new JSONObject();
+            userJSON.put("id", user.getId());
+            userJSON.put("firstName", user.getFirstName());
+            userJSON.put("lastName", user.getLastName());
+            userJSON.put("email", user.getEmail());
+            userArray.put(userJSON);
+        }
+        return userArray.toString();
     }
 }
