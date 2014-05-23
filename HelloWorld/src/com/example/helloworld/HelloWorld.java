32285572858a644ab57ed1e35f1322165a735d29
David Ding <chengjie.ding@gmail.com>
2014-05-23 16:22:33 +0800

    Creating and Running First Java Application

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by 908869 on 5/22/2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
